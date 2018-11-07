02d221e0cbfa5ecf4300cd8a864e7eb2d8dcfea7
mx@E <mglxp5@gmail.com>
2018-11-07 14:31:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear();
+  }
+}
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled(){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let ew = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+    let ns = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    return ns + ew;
+  }
+
+  estimatedTime(peak=false){
+    let b = this.blocksTravelled();
+    if (peak){
+      return b / 2;
+    } else {
+      return b / 3;
+    }
+  }
+}
